dea43a3fcb93ce3baa54ff3ad84b891fbf5a9895
granen32 <rnjsrbgud@naver.com>
2023-09-24 18:24:22 +0900

    [FEAT] common 작업

--- src/pages/Layout/style.tsx
+++ src/pages/Layout/style.tsx
@@ -1,7 +1,10 @@
 import styled from "styled-components";
 import { Container } from "@mui/material";
 
-export const LayoutPageContainer = styled(Container)``;
+export const LayoutPageContainer = styled.div`
+  margin: 0 auto;
+  font-size: 0;
+`;
 export const LayoutPageContent = styled.div`
   display: flex;
   flex-direction: column;
