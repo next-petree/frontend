90cceaec5a7705330b8fbfbd5e757880c4608b9f
ahn0min <ahn0min95@gmail.com>
2023-09-27 02:57:50 +0900

    feat: 자주 사용되는 스타일드 컴포넌트와 이미지 컴포넌트 추가

---
+++ src/component/0min/Center.tsx
@@ -0,0 +1,20 @@
+import styled from "@emotion/styled";
+// import { PropsWithChildren } from "react";
+
+// export const Center = ({ children }: PropsWithChildren) => {
+//   return <S.Center>{children}</S.Center>;
+// };
+
+export const Center = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+// const S = {
+//   Center: styled.div`
+//     display: flex;
+//     justify-content: center;
+//     align-items: center;
+//   `,
+// };
