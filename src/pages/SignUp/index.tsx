a05fab5cd4d09fc98cc57749543bd2857ba6b7b8
ahn0min <ahn0min95@gmail.com>
2023-09-26 23:54:51 +0900

    feat: 회원가입 페이지 라우팅 적용

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,9 @@
+// import { SignUpFormController } from "@component/SignUp/SignUpFormController";
+import { FC } from "react";
+
+const SignUpPage: FC = () => {
+  return <></>;
+  // return <SignUpFormController />;
+};
+
+export default SignUpPage;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,21 @@
 import LayoutPage from "@pages/Layout";
 import { lazy, FC } from "react";
 import { Routes, Route, Link, useRoutes } from "react-router-dom";
+import type { RouteObject } from "react-router-dom";
 
 const HomePage = lazy(() => import("@pages/Home/index"));
 const homeRoute = [
   {
     path: "home",
     element: <HomePage />,
+  },
+];
+
+const SignUpPage = lazy(() => import("@pages/SignUp/index"));
+const signUpRoute: RouteObject[] = [
+  {
+    path: "sign-up",
+    element: <SignUpPage />,
   },
 ];
 const RenderRouter: FC = () => {
@@ -16,6 +25,7 @@ const RenderRouter: FC = () => {
       element: <LayoutPage />,
       children: [
         ...homeRoute,
+        ...signUpRoute,
         // { 404 페이지 제작시 작업할 예정
         //   path: "*",
         //   element: (
