48c6cea98a6d204cbfe8cf1d457a2177c89a8742
ahn0min <ahn0min95@gmail.com>
2023-09-27 03:02:02 +0900

    feat: 테스트 결과 페이지 퍼블리싱 (성공 / 실패)

---
+++ src/pages/Test/Result/index.tsx
@@ -0,0 +1,12 @@
+import { TestResult } from "@component/Test/TestResult";
+
+/**
+ *
+ * 점수가 공개될시, 애니메이션 적용하여 점수조회 (정확히 어떤 애니메이션??)
+ *
+ */
+
+const TestResultPage = () => {
+  return <TestResult />;
+};
+export default TestResultPage;
