9d89dcfc0053e32d6359bb5cd36c36489379eda9
ahn0min <ahn0min95@gmail.com>
2023-10-01 02:11:37 +0900

    feat: 투명하게 감싸기만 하는 WrapButton Component 추가

---
+++ src/component/0min/WrapButton.tsx
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+export const WrapButton = styled.button`
+  background-color: transparent;
+  border: none;
+  padding: none;
+  margin: none;
+
+  :hover {
+    opacity: 0.8;
+    cursor: pointer;
+  }
+`;
