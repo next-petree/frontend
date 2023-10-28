f5fae832675e1533647876b10f7ce03e50390eb5
ChanghyeonO <ckdgus5189@gmail.com>
2023-10-29 01:23:10 +0900

    FEAT: 비밀번호 변경 및 확인 페이지 퍼블리싱 완료

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import FindEmail from "./pages/FindEmail/FindEmail";
 import FindPassword from "./pages/FindPassword/FindPassWord";
 import FindEmailResult from "./pages/FindEmailResult/FindEmailResult";
 import FindPasswordResult from "./pages/FindPasswordResult/FindPasswordResult";
+import ChangePassword from "./pages/ChangePassword/ChangePassword";
+import ChangePasswordResult from "./pages/ChangePasswordResult/ChangePasswordResult";
 
 function App() {
   return (
@@ -14,7 +16,12 @@ function App() {
         <Route path="/findemail" element={<FindEmail />} />
         <Route path="/findpassword" element={<FindPassword />} />
         <Route path="/findemailresult" element={<FindEmailResult />} />
-        <Route path="/findPasswordresult" element={<FindPasswordResult />} />
+        <Route path="/findpasswordresult" element={<FindPasswordResult />} />
+        <Route path="/changepassword" element={<ChangePassword />} />
+        <Route
+          path="/changepasswordresult"
+          element={<ChangePasswordResult />}
+        />
       </Routes>
     </>
   );
