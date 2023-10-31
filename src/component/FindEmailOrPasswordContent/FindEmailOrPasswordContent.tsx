9891f3bc4dc650ad1951f721b0436b263a76ebb8
ChanghyeonO <ckdgus5189@gmail.com>
2023-10-31 22:18:19 +0900

    FEAT: 이메일 찾기 기능 성공

--- src/component/FindEmailOrPasswordContent/FindEmailOrPasswordContent.tsx
+++ src/component/FindEmailOrPasswordContent/FindEmailOrPasswordContent.tsx
@@ -1,18 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { post } from "../../api/api";
+
 import {
   Container,
   ContentArea,
   FindEmailOrPasswordTitle,
   CharacterImage,
   InnerContentArea,
-  NameFormArea,
-  PhoneNumberFormArea,
-  PhoneNumberCheckFormArea,
+  NameInputArea,
+  PhoneNumberInputArea,
+  PhoneNumberCheckInputArea,
   NameText,
   PhoneNumberText,
-  NameForm,
-  PhoneNumberForm,
-  PhoneNumberCheckForm,
+  NameInput,
+  PhoneNumberInput,
+  PhoneNumberCheckInput,
   PhoneNumberSendButton,
   PhoneNumberCheckButton,
   FindPasswordButtonArea,
@@ -26,7 +29,97 @@ type Props = {
   pageType: "findemail" | "findpassword";
 };
 
+type ApiResponse = {
+  status: "SUCCESS" | "FAIL";
+  data: {
+    email?: string;
+    code?: string;
+  };
+};
+
 const FindEmailOrPasswordContent = ({ pageType }: Props) => {
+  const [nickname, setNickname] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [verificationCode, setVerificationCode] = useState("");
+  const [phoneNumberChecked, setPhoneNumberChecked] = useState(false);
+  const navigate = useNavigate();
+
+  const sendSMS = async () => {
+    try {
+      const response = await post<ApiResponse>("/api/sms/send", {
+        to: phoneNumber,
+      });
+
+      if (response.status === 200) {
+        alert("인증 번호가 발송되었습니다.");
+      } else {
+        alert("문자 발송에 실패하였습니다.");
+      }
+    } catch (error) {
+      console.error("SMS 발송 에러:", error);
+      alert("문자 발송 중 오류가 발생하였습니다.");
+    }
+  };
+
+  const formatPhoneNumber = (value: string) => {
+    const cleaned = ("" + value).replace(/\D/g, "");
+    const match = cleaned.match(/^(\d{3})(\d{4})(\d{4})$/);
+    if (match) {
+      return `${match[1]}-${match[2]}-${match[3]}`;
+    }
+    return value;
+  };
+
+  const handlePhoneNumberChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const formattedValue = formatPhoneNumber(e.target.value);
+    setPhoneNumber(formattedValue);
+  };
+
+  const verifySMS = async () => {
+    try {
+      const response = await post<ApiResponse>("/api/sms/verify", {
+        phoneNumber: phoneNumber,
+        code: verificationCode,
+      });
+
+      if (response.data.status === "SUCCESS") {
+        alert("인증에 성공하였습니다!");
+        setPhoneNumberChecked(true);
+      } else if (
+        response.data.status === "FAIL" &&
+        typeof response.data.data !== "string"
+      ) {
+        alert(response.data.data.code);
+      }
+    } catch (error) {
+      console.error("SMS 인증 에러:", error);
+      alert("인증 중 오류가 발생하였습니다.");
+    }
+  };
+
+  const findEmail = async () => {
+    try {
+      const response = await post<ApiResponse>("/api/email/find", {
+        nickname: nickname,
+        phoneNumberChecked,
+      });
+
+      if (response.data.status === "SUCCESS") {
+        navigate("/findemailresult", {
+          state: { email: response.data.data.email },
+        });
+      } else if (
+        response.data.status === "FAIL" &&
+        typeof response.data.data !== "string"
+      ) {
+        alert(response.data.data.code);
+      }
+    } catch (error) {
+      console.error("이메일 찾기 에러:", error);
+      alert("이메일 찾기 중 오류가 발생하였습니다.");
+    }
+  };
+
   return (
     <Container>
       <FindEmailOrPasswordTitle>
@@ -35,21 +128,34 @@ const FindEmailOrPasswordContent = ({ pageType }: Props) => {
       <ContentArea>
         <CharacterImage />
         <InnerContentArea>
-          <NameFormArea>
+          <NameInputArea>
             <NameText>이름</NameText>
-            <NameForm></NameForm>
-          </NameFormArea>
+            <NameInput
+              value={nickname}
+              onChange={e => setNickname(e.target.value)}
+            />
+          </NameInputArea>
 
-          <PhoneNumberFormArea>
+          <PhoneNumberInputArea>
             <PhoneNumberText>연락처</PhoneNumberText>
-            <PhoneNumberForm></PhoneNumberForm>
-            <PhoneNumberSendButton>인증 요청</PhoneNumberSendButton>
-          </PhoneNumberFormArea>
+            <PhoneNumberInput
+              value={phoneNumber}
+              onChange={handlePhoneNumberChange}
+            ></PhoneNumberInput>
+            <PhoneNumberSendButton onClick={sendSMS}>
+              인증 요청
+            </PhoneNumberSendButton>
+          </PhoneNumberInputArea>
 
-          <PhoneNumberCheckFormArea>
-            <PhoneNumberCheckForm></PhoneNumberCheckForm>
-            <PhoneNumberCheckButton>인증 확인</PhoneNumberCheckButton>
-          </PhoneNumberCheckFormArea>
+          <PhoneNumberCheckInputArea>
+            <PhoneNumberCheckInput
+              value={verificationCode}
+              onChange={e => setVerificationCode(e.target.value)}
+            ></PhoneNumberCheckInput>
+            <PhoneNumberCheckButton onClick={verifySMS}>
+              인증 확인
+            </PhoneNumberCheckButton>
+          </PhoneNumberCheckInputArea>
 
           <FindPasswordButtonArea>
             {pageType === "findemail" ? (
@@ -64,7 +170,7 @@ const FindEmailOrPasswordContent = ({ pageType }: Props) => {
       </ContentArea>
 
       <CheckButtonArea>
-        <CheckButton>확인</CheckButton>
+        <CheckButton onClick={findEmail}>확인</CheckButton>
       </CheckButtonArea>
     </Container>
   );
