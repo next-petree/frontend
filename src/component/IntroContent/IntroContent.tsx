5a9275fa828705492ff2267cbdf3f3fea3ea8bac
ChanghyeonO <ckdgus5189@gmail.com>
2023-11-01 23:35:07 +0900

    FEAT: 펫트리 인트로 페이지 완료

--- src/component/IntroContent/IntroContent.tsx
+++ src/component/IntroContent/IntroContent.tsx
@@ -16,6 +16,27 @@ import {
   IntroArea3MainTitle,
   IntroArea3SubTitle,
   IntroArea3Introduce,
+  IntroArea4,
+  IntroArea4MainTitle,
+  IntroArea4SubTitle,
+  ImageContainer,
+  LeftContent,
+  LeftImage,
+  LeftHashTag,
+  LeftImageIntroduce,
+  RightContent,
+  RightImage,
+  RightHashTag,
+  RightImageIntroduce,
+  IntroArea5,
+  IntroArea5MainTitle,
+  IntroArea5SubTitle,
+  IntroArea5HashTagArea,
+  HashTag1,
+  HashTag2,
+  HashTag3,
+  HashTag4,
+  HashTag5,
 } from "./IntroContentStyle";
 
 const IntroContent = () => {
@@ -25,6 +46,7 @@ const IntroContent = () => {
         <TitleLogo />
         <TitleTextLogo />
       </IntroArea1>
+
       <IntroArea2>
         <IntroArea2DogImage />
         <IntroArea2TextArea>
@@ -46,6 +68,7 @@ const IntroContent = () => {
           </IntroArea2Introduce>
         </IntroArea2TextArea>
       </IntroArea2>
+
       <IntroArea3>
         <IntroArea3ContentBox>
           <IntroArea3MainTitle>펫트리</IntroArea3MainTitle>
@@ -59,8 +82,55 @@ const IntroContent = () => {
           </IntroArea3Introduce>
         </IntroArea3ContentBox>
       </IntroArea3>
-      {/* <IntroArea4></IntroArea4>
-      <IntroArea5></IntroArea5> */}
+
+      <IntroArea4>
+        <IntroArea4MainTitle>
+          새로운 당신의 가족,
+          <br /> 반려동물을 맞이하세요!
+        </IntroArea4MainTitle>
+        <IntroArea4SubTitle>
+          사랑스러운 반려견을 따뜻한 가족의 품으로 함께 동참하세요!
+        </IntroArea4SubTitle>
+
+        <ImageContainer>
+          <LeftContent>
+            <LeftImage>
+              <LeftHashTag>#브리더</LeftHashTag>
+            </LeftImage>
+            <LeftImageIntroduce>
+              반려견의 속마음을
+              <br />잘 아는 브리더
+            </LeftImageIntroduce>
+          </LeftContent>
+          <RightContent>
+            <RightImage>
+              <RightHashTag>#분양희망자</RightHashTag>
+            </RightImage>
+            <RightImageIntroduce>
+              반려견을 잘 케어해 줄<br />
+              분양희망자
+            </RightImageIntroduce>
+          </RightContent>
+        </ImageContainer>
+      </IntroArea4>
+
+      <IntroArea5>
+        <IntroArea5MainTitle>
+          펫트리와 함께,
+          <br />
+          분양 서비스를 시작하세요!
+        </IntroArea5MainTitle>
+        <IntroArea5SubTitle>
+          섬세한 브리더의 손길로 탄생한 반려견과 함께하세요!
+        </IntroArea5SubTitle>
+        <IntroArea5HashTagArea>
+          <HashTag1>#펫트리</HashTag1>
+          <HashTag2>#브리더</HashTag2>
+          <HashTag3>#반려동물</HashTag3>
+          <HashTag4>#댕댕이</HashTag4>
+          <HashTag5>#가족</HashTag5>
+        </IntroArea5HashTagArea>
+      </IntroArea5>
     </Container>
   );
 };
--- src/component/IntroContent/IntroContentStyle.ts
+++ src/component/IntroContent/IntroContentStyle.ts
@@ -4,7 +4,14 @@ import firstBackgrondImage from "../../assets/images/Rectangle9.png";
 import petTreeLogoImage from "../../assets/images/PetreeIconWhite.png";
 import petTreeTextImage from "../../assets/images/PetreeTitleBig.png";
 
-import FirstDogImage from "../../assets/images/Layer41.png";
+import FirstDogsImage from "../../assets/images/Layer41.png";
+
+import SecondDogsImage from "../../assets/images/Layer31.png";
+
+import LeftDogImage from "../../assets/images/Vector1.png";
+import RightDogImage from "../../assets/images/Vector2.png";
+
+import ThirdDogsImage from "../../assets/images/Rectangle7.png";
 
 export const Container = styled.div`
   display: flex;
@@ -45,7 +52,7 @@ export const IntroArea2 = styled.div`
 export const IntroArea2DogImage = styled.div`
   width: 480px;
   height: 440px;
-  background: url(${FirstDogImage}) no-repeat center;
+  background: url(${FirstDogsImage}) no-repeat center;
 `;
 
 export const IntroArea2TextArea = styled.div`
@@ -101,13 +108,255 @@ export const IntroArea2Introduce = styled.div`
 
 export const IntroArea3 = styled.div`
   height: 793px;
-  background: url(${FirstDogImage}) no-repeat center;
+  background: url(${SecondDogsImage}) no-repeat center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
-export const IntroArea3ContentBox = styled.div``;
+export const IntroArea3ContentBox = styled.div`
+  width: 570px;
+  height: 250px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  padding: 24px 60px 24px 60px;
+  border-radius: 30px;
+  background: rgba(255, 255, 255, 0.65);
+`;
 
-export const IntroArea3MainTitle = styled.div``;
+export const IntroArea3MainTitle = styled.div`
+  color: #4a4a4a;
+  text-align: center;
+  font-family: Noto Sans KR;
+  font-size: 40px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 50px;
+  letter-spacing: -2px;
+`;
 
-export const IntroArea3SubTitle = styled.div``;
+export const IntroArea3SubTitle = styled.div`
+  color: #4a4a4a;
+  text-align: center;
+  font-family: Noto Sans KR;
+  font-size: 40px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 50px;
+  letter-spacing: -2px;
+  margin-bottom: 30px;
+`;
 
-export const IntroArea3Introduce = styled.div``;
+export const IntroArea3Introduce = styled.div`
+  color: #636363;
+  text-align: center;
+  font-family: Noto Sans KR;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  letter-spacing: -1.2px;
+`;
+
+export const IntroArea4 = styled.div`
+  height: 1000px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export const IntroArea4MainTitle = styled.div`
+  color: #4ec1bf;
+  text-align: center;
+  font-family: Noto Sans KR;
+  font-size: 40px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 54px;
+  letter-spacing: -2px;
+`;
+
+export const IntroArea4SubTitle = styled.div`
+  color: #4ec1bf;
+  text-align: center;
+  font-family: Noto Sans KR;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 42px;
+  letter-spacing: -1.2px;
+  margin-bottom: 50px;
+`;
+
+export const ImageContainer = styled.div`
+  display: flex;
+  gap: 20px;
+`;
+
+export const LeftContent = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export const LeftImage = styled.div`
+  width: 480px;
+  height: 340px;
+  background: url(${LeftDogImage}) no-repeat center;
+  border-radius: 15px;
+  margin-bottom: 20px;
+`;
+
+export const LeftHashTag = styled.div`
+  width: 60px;
+  height: 30px;
+  padding: 3px 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50px;
+  border: 2px solid #4ec1bf;
+  background: #fff;
+  margin-top: 20px;
+  margin-left: 20px;
+  color: #4ec1bf;
+  text-align: center;
+  font-family: Noto Sans KR;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  letter-spacing: -0.8px;
+`;
+
+export const LeftImageIntroduce = styled.div`
+  color: #292929;
+  text-align: center;
+  font-family: Noto Sans KR;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  letter-spacing: -1.2px;
+`;
+
+export const RightContent = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export const RightImage = styled.div`
+  width: 480px;
+  height: 340px;
+  background: url(${RightDogImage}) no-repeat center;
+  border-radius: 15px;
+  margin-bottom: 20px;
+`;
+
+export const RightHashTag = styled.div`
+  width: 80px;
+  height: 30px;
+  padding: 3px 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50px;
+  border: 2px solid #4ec1bf;
+  background: #fff;
+  margin-top: 20px;
+  margin-left: 20px;
+  color: #4ec1bf;
+  text-align: center;
+  font-family: Noto Sans KR;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  letter-spacing: -0.8px;
+`;
+
+export const RightImageIntroduce = styled.div`
+  color: #292929;
+  text-align: center;
+  font-family: Noto Sans KR;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  letter-spacing: -1.2px;
+`;
+
+export const IntroArea5 = styled.div`
+  height: 717px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: url(${ThirdDogsImage}) no-repeat center;
+`;
+
+export const IntroArea5MainTitle = styled.div`
+  color: #fff;
+  text-align: center;
+  font-family: Noto Sans KR;
+  font-size: 40px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 54px;
+  letter-spacing: -2px;
+  margin-bottom: 20px;
+`;
+
+export const IntroArea5SubTitle = styled.div`
+  color: #fff;
+  text-align: center;
+  font-family: Noto Sans KR;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 36px;
+  letter-spacing: -1.2px;
+  margin-bottom: 30px;
+`;
+
+export const IntroArea5HashTagArea = styled.div`
+  display: flex;
+  gap: 15px;
+`;
+
+const HashTag = styled.div`
+  color: #fff;
+  text-align: center;
+  font-family: Noto Sans KR;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 42px;
+  letter-spacing: -1px;
+  border-radius: 50px;
+  border: 2px solid #fff;
+  background: rgba(255, 255, 255, 0);
+`;
+
+export const HashTag1 = styled(HashTag)`
+  width: 100px;
+`;
+
+export const HashTag2 = styled(HashTag)`
+  width: 100px;
+`;
+
+export const HashTag3 = styled(HashTag)`
+  width: 120px;
+`;
+
+export const HashTag4 = styled(HashTag)`
+  width: 100px;
+`;
+
+export const HashTag5 = styled(HashTag)`
+  width: 80px;
+`;
