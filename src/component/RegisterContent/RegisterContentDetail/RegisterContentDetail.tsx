f81d4d27b3b3642913da33f369a062b83b655631
ChanghyeonO <ckdgus5189@gmail.com>
2023-11-20 01:35:54 +0900

    FEAT: 회원가입 첫페이지 완성

---
+++ src/component/RegisterContent/RegisterContentDetail/RegisterContentDetail.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RegisterContentDetail = () => {
+  return;
+};
+
+export default RegisterContentDetail;
--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../../component/Header/Header";
+import RegisterContent from "../../component/RegisterContent/RegisterContent/RegisterContent";
 
 const Register = () => {
   return (
     <>
       <Header />
+      <RegisterContent />
     </>
   );
 };
