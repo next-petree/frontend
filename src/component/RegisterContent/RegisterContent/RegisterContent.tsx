7cef7f326e0b7f4c21388f0c9363f78f5cf15469
ChanghyeonO <ckdgus5189@gmail.com>
2023-11-24 02:21:33 +0900

    FEAT: 약관 전문 내용 추가

--- src/component/RegisterContent/RegisterContent/RegisterContent.tsx
+++ src/component/RegisterContent/RegisterContent/RegisterContent.tsx
@@ -33,6 +33,7 @@ import {
 } from "./RegisterContentStyle";
 
 import DropDownButton from "../../../assets/icons/Group 5.png";
+import DropUpButton from "../../../assets/icons/Group 51.png";
 import RegisterSequenceImage from "../../../assets/images/register-sequence.png";
 
 const RegisterContent = () => {
@@ -42,6 +43,24 @@ const RegisterContent = () => {
     personalInformation: false,
     locationBasedService: false,
   });
+
+  const [isTermsDropdownOpen, setIsTermsDropdownOpen] = useState(false);
+  const [isPersonalInfoDropdownOpen, setIsPersonalInfoDropdownOpen] =
+    useState(false);
+  const [isLocationServiceDropdownOpen, setIsLocationServiceDropdownOpen] =
+    useState(false);
+
+  const toggleTermsDropdown = () => {
+    setIsTermsDropdownOpen(!isTermsDropdownOpen);
+  };
+
+  const togglePersonalInfoDropdown = () => {
+    setIsPersonalInfoDropdownOpen(!isPersonalInfoDropdownOpen);
+  };
+
+  const toggleLocationServiceDropdown = () => {
+    setIsLocationServiceDropdownOpen(!isLocationServiceDropdownOpen);
+  };
 
   const handleAgreeAllChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const checked = event.target.checked;
@@ -114,9 +133,35 @@ const RegisterContent = () => {
             />
             <TermsTitle>이용약관</TermsTitle>
           </TermsContent>
-          <DropDown>
-            <span>약관 전문 보기</span>
-            <img src={DropDownButton} alt="DropDown button" />
+          <DropDown onClick={toggleTermsDropdown}>
+            {!isTermsDropdownOpen && (
+              <>
+                <span>약관 전문 보기</span>
+                <img src={DropDownButton} alt="DropDown button" />
+              </>
+            )}
+            {isTermsDropdownOpen && (
+              <>
+                <span>
+                  이용약관
+                  <br />
+                  <br /> 제 1 조 (목적) 본 약관은 {"<펫트리>"}(이하
+                  '펫트리')은(는)의 제반 서비스의 이용조건 및 절차에 관한 사항
+                  및 기타 필요한 사항을 규정함을 목적으로 합니다.
+                  <br />제 2 조 (용어의 정의) 본 약관에서 사용하는 용어는 다음과
+                  같이 정의 합니다.
+                  <br />
+                  ①회원 : 기본 회원 정보를 입력하였고, 회사와 서비스 이용계약을
+                  체결하여 아이디를 부여받은 개인 <br />
+                  ②아이디(ID) : 회원식별과 회원의 서비스 이용을 위해 회원이
+                  선정하고 회사가 승인하는 문자와 숫자의 조합
+                  <br />
+                  ③비밀번호(Password) : 회원이 통신상의 자신의 비밀을 보호하기
+                  위해 선정한 문자와 숫자의 조합
+                </span>
+                <img src={DropUpButton} alt="DropUp button" />
+              </>
+            )}
           </DropDown>
         </TermsArea>
         <PersonalInfomationArea>
@@ -130,9 +175,34 @@ const RegisterContent = () => {
               개인정보 수집 및 이용
             </PersonalInfomationTitle>
           </PersonalInfomationContent>
-          <DropDown>
-            <span>약관 전문 보기</span>
-            <img src={DropDownButton} alt="DropDown button" />
+          <DropDown onClick={togglePersonalInfoDropdown}>
+            {!isPersonalInfoDropdownOpen && (
+              <>
+                <span>약관 전문 보기</span>
+                <img src={DropDownButton} alt="DropDown button" />
+              </>
+            )}
+            {isPersonalInfoDropdownOpen && (
+              <>
+                <span>
+                  개인정보 수집 및 이용
+                  <br />
+                  <br /> 제1조(개인정보의 처리 목적) 이하 '펫트리'은(는) 다음의
+                  목적을 위하여 개인정보를 처리합니다.
+                  <br />
+                  처리하고 있는 개인정보는 다음의 목적 이외의 용도로는 이용되지
+                  않으며 이용 목적이 변경되는 경우에는 「개인정보 보호법」
+                  제18조에 따라 별도의 동의를 받는 등 필요한 조치를 이행할
+                  예정입니다. <br />
+                  1. 홈페이지 회원가입 및 관리 회원 가입의사 확인, 회원제 서비스
+                  제공에 따른 본인 식별·인증, 회원자격 유지·관리, 서비스
+                  부정이용 방지, 만14세 미만 아동의 개인정보 처리 시
+                  법정대리인의 동의여부 확인, 각종 고지·통지 목적으로 개인정보를
+                  처리합니다.
+                </span>
+                <img src={DropUpButton} alt="DropUp button" />
+              </>
+            )}
           </DropDown>
         </PersonalInfomationArea>
         <LocationBasedServiceArea>
@@ -146,9 +216,32 @@ const RegisterContent = () => {
               위치기반서비스 이용 약관
             </LocationBasedServiceTitle>
           </LocationBasedServiceContent>
-          <DropDown>
-            <span>약관 전문 보기</span>
-            <img src={DropDownButton} alt="DropDown button" />
+          <DropDown onClick={toggleLocationServiceDropdown}>
+            {!isLocationServiceDropdownOpen && (
+              <>
+                <span>약관 전문 보기</span>
+                <img src={DropDownButton} alt="DropDown button" />
+              </>
+            )}
+            {isLocationServiceDropdownOpen && (
+              <>
+                <span>
+                  개인정보 제 3자 제공 동의 <br /> <br /> 제 1 조 (목적) 본
+                  약관은 {"<펫트리>"}(이하 '펫트리')은(는)의 제반 서비스의
+                  이용조건 및 절차에 관한 사항 및 기타 필요한 사항을 규정함을
+                  목적으로 합니다. <br />제 2 조 (용어의 정의) 본 약관에서
+                  사용하는 용어는 다음과 같이 정의 합니다. <br /> ①회원 : 기본
+                  회원 정보를 입력하였고, 회사와 서비스 이용계약을 체결하여
+                  아이디를 부여받은 개인 <br />
+                  ②아이디(ID) : 회원식별과 회원의 서비스 이용을 위해 회원이
+                  선정하고 회사가 승인하는 문자와 숫자의 조합
+                  <br />
+                  ③비밀번호(Password) : 회원이 통신상의 자신의 비밀을 보호하기
+                  위해 선정한 문자와 숫자의 조합
+                </span>
+                <img src={DropUpButton} alt="DropUp button" />
+              </>
+            )}
           </DropDown>
         </LocationBasedServiceArea>
       </InnerContent>
