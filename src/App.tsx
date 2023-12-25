7173fbb0c6f1af0508c9891f6dc8ac631cd391bb
ChanghyeonO <ckdgus5189@gmail.com>
2023-12-25 22:40:19 +0900

    fix: 대소문자 구분 문제 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
-import BreederCollect from "./pages/Collects1/BreederCollect1";
-import DogyCollect from "./pages/Collects1/DogyCollect1";
+import BreederCollect from "./pages/Collects/BreederCollect";
+import DogyCollect from "./pages/Collects/DogyCollect";
 import BreedingRegister from "./pages/breeding-register/register/BreedingRegister";
 import RegisterFillOut from "./pages/breeding-register/fillout/RegisterFillOut";
 import BreedingComplete from "./pages/breeding-register/complete/BreedingComplete";
